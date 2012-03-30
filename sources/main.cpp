1cacdda45b7d6ab8f0ab7bb5b62d553dac7b2c23
oscar <oscar@oscar.(none)>
2012-03-30 12:34:47 +0200

    host simulation and shutdown of hilaris and oscar

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,42 +7,12 @@ int main(){
 	Hilaris hilaris;
 	
 	hilaris.setConsoleLogLevel(DEBUG);
+	hilaris.setFileLogLevel(NONE);
 	
-	/*
 	Camera* camera = hilaris.getCamera();
-	
-	OscLog(DEBUG, "Error: %d\n", camera->getLastError());
-	
-	Image* image = camera->captureImage();
+	Image*  image = camera->captureImage();
 		
-	image->save("/home/httpd/1.bmp");
-	
-	image->filter(&GAUSS3X3);
-	
-	image->save("/home/httpd/2.bmp");
-	
-	printf("%p", image);
-	*/
-	
-	StreamServer srv;
-	
-	srv.start();
-	
-	int i = 0;
-	
-	while(1)
-	{
-		char msg[100];
-		
-		sprintf(msg, "message nr: %d", i++);
-		
-		srv.message = msg;
-		usleep(4000);
-	}
-	
-	srv.stop();
-	
-	
+	image->save("foo.bmp");
 	
 	return 0;
 }
