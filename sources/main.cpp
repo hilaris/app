c20841b4c857e5b7539dbe8004c32528cd5d7796
oscar <oscar@oscar.(none)>
2012-03-30 14:36:35 +0200

    cleanup

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,43 +7,15 @@ int main(){
 	
 	Hilaris hilaris;
 	
-	hilaris.setConsoleLogLevel(DEBUG);
+	hilaris.setConsoleLogLevel(NONE);
 	hilaris.setFileLogLevel(NONE);
 	
 	Camera* camera = hilaris.getCamera();
-
-	/*	
-	OscLog(DEBUG, "Error: %d\n", camera->getLastError());
+	Image*  image = camera->captureImage();
 	
-	Image* image = camera->captureImage();
-		
-	image->save("/home/httpd/1.bmp");
+	image->toGreyscale();
 	
-	image->filter(&GAUSS3X3);
-	
-	image->save("/home/httpd/2.bmp");
-	
-	printf("%p", image);
-	*/
-	
-	StreamServer srv;
-	
-	srv.start();
-	
-	int i = 0;
-	
-	while(1)
-	{
-		char msg[100];
-		
-		sprintf(msg, "message nr: %d", i++);
-		Image*  image = camera->captureImage();
-
-		srv.insertImage(*image);
-		usleep(4000);
-	}
-	
-	srv.stop();
+	image->save("foo.bmp");
 	
 	return 0;
 }
