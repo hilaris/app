7c6054a0e072bc15942537097e0cc04a67435ada
oscar <oscar@oscar.(none)>
2012-04-03 10:28:49 +0200

    greyscale debayer, osc_picture fixed to instance

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,9 @@
 
+#include "oscar.h"
 #include "Hilaris.h"
 #include "StreamServer.h"
 #include "Image.h"
+#include "GreyscaleImageFactory.h"
 #include "BGRImageFactory.h"
 
 int main(){
@@ -12,22 +14,13 @@ int main(){
 	Camera* camera = hilaris.getCamera(OSC_PICTURE_GREYSCALE);
 	camera->setAutoExposure(true);	
 	
-	RawImage* img   = camera->captureImage();
-	BGRImage  image = BGRImageFactory::getHalfsizeDebayered(img);
+	RawImage* img  = camera->captureImage();
+	GreyscaleImage image = GreyscaleImageFactory::getFastDebayered(img);
 	
 	image.save("meep.bmp");
-	
-	//Image*  image = camera->captureImage();
-	
-	//image->toGreyscale();
-	
-	//image->save("foo.bmp");
-	
-	//StreamServer srv(camera);
-	
-	//srv.start();
-	
-	//while(1);
+
+	//GreyscaleImage GreyscaleImageFactory::getVectorDebayered(RawImage* raw)
+	//GreyscaleImage GreyscaleImageFactory::getHalfsizeDebayered(RawImage* raw)
 	
 	return 0;
 }
