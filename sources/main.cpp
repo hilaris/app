82bbdbc2af2ba9a43b7d3e36b7c78782f2aeb46a
oscar <oscar@oscar.(none)>
2012-04-10 16:23:21 +0200

    allow to change image type in processor

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,14 +1,16 @@
 #include "Hilaris.h"
 
+#include "processors/BinaryProcessor.h"
+
 int main(){
 	
 	Hilaris hilaris;
 	hilaris.setConsoleLogLevel(DEBUG);
 	
 
-	Camera* cam = hilaris.getCamera(new DebayerBGRFast());
+	Camera* cam = hilaris.getCamera(new DebayerGreyscaleFast());
 	
-	//cam->addFrameProcessor(new BinaryProcessor());
+	cam->addFrameProcessor(new BinaryProcessor());
 	
 	StreamServer s(cam);
 	s.start();
