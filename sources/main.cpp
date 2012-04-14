e405d2d4aab56245273792cbf05d9f31854c9f99
Jim Schmid <devsheeep@gmail.com>
2012-04-14 18:35:18 +0200

    doxygen include pathes and camera documented

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,16 +1,16 @@
 #include "Hilaris.h"
+#include "processors/RegionProcessors.h"
 
 int main(){
 	
 	Hilaris hilaris;
 	hilaris.setConsoleLogLevel(DEBUG);
 	
-	Camera* cam = hilaris.getCamera(0, 0, 100, 100, new DebayerBGRFast());
+	Camera* cam = hilaris.getCamera(new DebayerGreyscaleFast());
+	cam->addFrameProcessor(new RegionProcessors());
 	
-	BGRImage* bgr = (BGRImage*)cam->captureImage();
-	
-	// save
-	bgr->save("aoi.bmp");
+	StreamServer srv(cam);
+	srv.start();
 	
 	return 0;
 }
