7cb8505c795d8e0cd25432fbf6d8e1ea5b10f6ef
oscar <oscar@oscar.(none)>
2012-04-16 08:45:13 +0200

    test app

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,13 +1,18 @@
 #include "Hilaris.h"
 
-#include "processors/BinaryProcessor.h"
+#include "processors/TestProcessor.h"
 
 int main(){
 	
 	Hilaris hilaris;
-	hilaris.setConsoleLogLevel(DEBUG);
-	
+	hilaris.setConsoleLogLevel(NONE);
 
+	Camera* cam = hilaris.getCamera(new DebayerBGRFast());
+	
+	cam->addFrameProcessor(new TestProcessor());
+
+	StreamServer s(cam);
+	s.start();
 	
 	return 0;
 }
