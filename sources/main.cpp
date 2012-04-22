00fe8a3d9bfde7ae0ec99c39076dbfa7d6920585
oscar <oscar@oscar.(none)>
2012-04-22 21:49:48 +0200

    image buffer improvements

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,17 +1,18 @@
 #include "Hilaris.h"
 
+#include "processors/EdgeProcessor.h"
+
 int main(){
 	
 	Hilaris hilaris;
-	
 	hilaris.setConsoleLogLevel(DEBUG);
 	
 	Camera* cam = hilaris.getCamera(new DebayerGreyscaleFast());
-	cam->setAutoExposure(true);
+	cam->setAutoExposure(true);	
+	cam->addFrameProcessor(new EdgeProcessor());
 	
 	StreamServer s(cam);
 	s.start();
-
 	
 	return 0;
 }
