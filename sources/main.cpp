ce2a2e5ebbeee211ac3e90a00b2c20c550d37dc1
Jim Schmid <devsheeep@gmail.com>
2012-04-23 18:19:55 +0200

    ov init

--- sources/main.cpp
+++ sources/main.cpp
@@ -5,11 +5,14 @@ int main(){
 	
 	Hilaris hilaris;
 	hilaris.setConsoleLogLevel(DEBUG);
-	
+		
 	BGRImage image = BGRImageFactory::create(200, 200, 255, 255, 255);
 	
-	Text* t = new Text(10, 20, 1, "foo", 0x00FF00, 0x0000FF);
+	Text* t = new Text(10, 20, 1, "fmepoo", 0x00FF00, 0x0000FF);
 	t->draw(&image);
+	
+	Box* b = new Box(10, 20, 30, 40, 0xFF0000);
+	b->draw(&image);
 	
 	image.save("overlay-fancy.bmp");
 	
