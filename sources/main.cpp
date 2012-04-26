e4a30d59a4e5e8ae203c6d25ac2e498df34fba03
Michael Kreis <michi.kreis@gmail.com>
2012-04-26 17:08:03 +0200

    new test image

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,20 +1,18 @@
 #include "Hilaris.h"
-#include "processors/FingertipDetection.h"
-#include "processors/BallProcessor.h"
 
 int main(){
 
 	Hilaris hilaris;
 	hilaris.setConsoleLogLevel(INFO);	
 
-	Camera* cam = hilaris.getCamera(new DebayerGreyscaleFast());
+	Camera* cam = hilaris.getCamera(new DebayerBGRBilinear());
 	cam->setAutoExposure(true);
 
-	cam->addFrameProcessor(new FingertipDetection());
-	cam->addFrameProcessor(new BallProcessor());
+	//cam->addFrameProcessor(new FingertipDetection());
+	//cam->addFrameProcessor(new BallProcessor());
 	
-	StreamServer s(cam);
-	s.start();
+	//StreamServer s(cam);
+	//s.start();
 	
 	return 0;
 }
